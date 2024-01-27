a8e04a934dc32003d77f109cf4cf560e4e25c971
DCZLLC <150105898+DCZLLC@users.noreply.github.com>
2024-01-26 21:57:13 -0500

    Update runupdate.sh

--- runupdate.sh
+++ runupdate.sh
@@ -8,48 +8,6 @@ echo "You can use the following command to stop the daemon:"
 echo "    cld stop"
 echo "========================="
 
-echo "Updating package list..."
-sudo apt update
-
-echo "Installing dependencies..."
-sudo apt install -y \
-  build-essential \
-  libssl-dev \
-  libboost-chrono1.74-dev \
-  libboost-filesystem1.74-dev \
-  libboost-program-options1.74-dev \
-  libboost-system1.74-dev \
-  libboost-thread1.74-dev \
-  libboost-test1.74-dev \
-  qtbase5-dev \
-  qttools5-dev \
-  bison \
-  libexpat1-dev \
-  libdbus-1-dev \
-  libfontconfig-dev \
-  libfreetype-dev \
-  libice-dev \
-  libsm-dev \
-  libx11-dev \
-  libxau-dev \
-  libxext-dev \
-  libevent-dev \
-  libxcb1-dev \
-  libxkbcommon-dev \
-  libminiupnpc-dev \
-  libprotobuf-dev \
-  libqrencode-dev \
-  xcb-proto \
-  x11proto-xext-dev \
-  x11proto-dev \
-  xtrans-dev \
-  zlib1g-dev \
-  libczmq-dev \
-  autoconf \
-  automake \
-  libtool \
-  protobuf-compiler
-
 echo "Setting up directory structure..."
 mkdir -p $HOME/src
 cd $HOME/src
